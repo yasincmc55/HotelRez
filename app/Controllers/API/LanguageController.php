593e88a53ea6cf76ff6ea4feb47871ef7379204b
yasincmc55 <yasincmc2012@gmail.com>
2024-11-04 17:46:57 +0300

    Language Add Process

--- app/Controllers/API/LanguageController.php
+++ app/Controllers/API/LanguageController.php
@@ -6,5 +6,32 @@ use App\Controllers\BaseController;
 use App\Models\LanguageModel;
 
 class LanguageController extends BaseController{
-    
+  public function language_save(){
+    $lang = new LanguageModel();
+
+    $name = $this->request->getPost('name');
+    $lang_code = $this->request->getPost('lang_code');
+
+    if(empty($name) || empty($lang_code)){
+      return $this->response->setJSON([
+        'status'=>'error',
+        'message'=>'Name Or lang code cant be NULL'
+      ]);
+    }
+
+
+    if($lang->insert(['name'=>$name , 'code'=>$lang_code])){
+      return $this->response->setJSON([
+        'status'=>'success',
+        'message'=>'Language saved successfully.'
+      ]);
+    }else{
+      return $this->response->setJSON([
+        'status'=>'error',
+        'message'=>'Failed to save language'
+      ]);
+    }
+
+
+  }
 }
